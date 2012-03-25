f75c1242e457ad0f6958a37d8f15f795be5364d3
Brent Miller <foliosus@foliosus.com>
2012-03-25 11:37:31 -0700

    Initial commit: bare rails app
    Comes with empty database.yml, but with a good .gitignore and .rvmrc

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gmi::Application.config.secret_token = 'b786b51591b9ee06b486c14f6f63c9979eb76458a5f44fb7ee3e6609387f5bd8b62595f68555a22f168d8c31a4838a65d8ef6a9f5b8f8c4f99621c7ec9d7d685'
